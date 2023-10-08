bb4e3089bfd972f0191ce1fd29d6b84eb4ad8fa6
Ahmadmarei2001 <81181915+Ahmadmarei2001@users.noreply.github.com>
2023-10-08 21:46:42 +0300

    updateAhmad

--- src/main.js
+++ src/main.js
@@ -49,18 +49,17 @@ function Main() {
       </div>
 
       {/* // about us */}
-      <section id="about-section">
-        <div>
-        <img id="about-img" src={AboutImage} alt="Logo" />
-            {/* <img src="./about.png" alt="about_img" />  */}
-        </div>
-        <div id="about-content">
-            <p id="about-word">About Us</p> 
-            <hr />
-            <p> Speed Car Rental is one of the largest car rental companies worldwide, available in the major airports in the world and operates more than 1,200 locations in more than 90 countries. Dollar Jordan one of the largest car rental offices in Jordan, we have in operations 2300 vehicles with all types and sizes, we do operate self-drive car rentals for short and long term also we do a leasing program and a chauffeur driven services. Our business mainly engaged in the business of the daily and yearly contracts for individuals, NGOs, embassies and US ARMY and German Army.</p>
-            <button src="" > Rent Now</button>
-        </div>
-    </section>
+      <section id="about-section" class="about-section">
+  <div class="about-img-container">
+    <img id="about-img" src={AboutImage} alt="Logo" />
+  </div>
+  <div id="about-content" class="about-content">
+    <p id="about-word">About Us</p>
+    <hr />
+    <p>Speed Car Rental is one of the largest car rental companies worldwide, available in the major airports in the world and operates more than 1,200 locations in more than 90 countries. Dollar Jordan one of the largest car rental offices in Jordan, we have in operations 2300 vehicles with all types and sizes, we do operate self-drive car rentals for short and long term also we do a leasing program and a chauffeur driven services. Our business mainly engaged in the business of the daily and yearly contracts for individuals, NGOs, embassies and US ARMY and German Army.</p>
+    <button src="">Rent Now</button>
+  </div>
+</section>
 
     {/* // contact us */}
     <section id="contact">
